5f87a52539733f872fd780f082f37a304aab049d
pjt3591oo <pjt3591oo@gmail.com>
2021-11-30 23:10:06 +0900

    openzeppelin erc20 코트 표준

--- contracts/openErc20.sol
+++ contracts/openErc20.sol
@@ -6,6 +6,18 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 contract openErc20 is ERC20 {
     constructor (string memory name, string memory symbol, uint8 decimals, uint totalSupply) 
     ERC20 (name, symbol) public {
-        _mint(msg.sender, totalSupply * (10 ** uint(decimals)));
+        _mint(msg.sender, totalSupply);
     }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {}
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {}
 }
